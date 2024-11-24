e8c1491184e648294b01557d2bb7e7a231db9486
TruestReaper <chirayiljohnny@gmail.com>
2024-11-24 16:59:50 -0600

    Imported and Completed Challenge 5

---
+++ Challenge 5/Challenge 5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,37 @@
+﻿/*
+* Name: John Chirayi
+* File: DifficultyButtonX.cs
+* Assignment 8 - Challenge 5
+* Description: This code allows the player
+* to choose whatever difficulty they want
+* for the game.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManagerX.StartGame(difficulty);
+    }
+}
